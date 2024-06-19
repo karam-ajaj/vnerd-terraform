40eab01ba3f774a48506f46d08a8658e98b12c17
Karam Ajaj <37446151+karam-ajaj@users.noreply.github.com>
2024-06-19 14:45:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc3"
+    }
+  }
+}
 
+provider "proxmox" {
+  pm_api_url = "http://10.10.10.110:8006/api2/json"
+}
