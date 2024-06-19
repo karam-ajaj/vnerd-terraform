01366d57adb5bdd2ceb63295aca94bfdfe49505b
Karam Ajaj <37446151+karam-ajaj@users.noreply.github.com>
2024-06-19 14:39:00 +0200

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc3"
+    }
+  }
+}
+
+provider "proxmox" {
+  # Configuration options
+}
