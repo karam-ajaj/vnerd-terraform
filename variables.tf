e7f868d2ee2b7518eded8de8b87e9e65c6200643
Karam Ajaj <37446151+karam-ajaj@users.noreply.github.com>
2024-06-20 19:24:43 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,35 @@
-variable "px_credentials" {
-  PM_USER = "terraform@pve"
-  PM_PASS = "29Kv%&acuWvM3eMb"
+variable "PM_API_URL" {
+  type    = string
+  default = "https://10.10.10.111:8006/api2/json"
 }
+
+variable "PM_USER" {
+  type    = string
+  default = "terraform-prov@pve"
+}
+
+variable "PM_PASS" {
+  type    = string
+  default = "29Kv%&acuWvM3eMb"
+}
+
+# variable "interface" {
+#   type = string
+#   default = <<EOF1
+#   network:
+#   ethernets:
+#     ens18:
+#       addresses:
+#       - 10.10.10.81/24
+#       nameservers:
+#         addresses:
+#         - 10.10.10.220
+#         - 8.8.8.8
+#         search:
+#         - karam.lab
+#       routes:
+#       - to: default
+#         via: 10.10.10.1
+#   version: 2
+#   EOF
+#   }
